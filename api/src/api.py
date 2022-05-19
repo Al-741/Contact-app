3d81f6134ad66ceb0c17748d21158fc6af848e7e
sabrineTT <sabrine.thibert@esme.fr>
2022-05-19 16:17:23 +0200

    Creation de contacts avec un api

--- api/src/api.py
+++ api/src/api.py
@@ -6,6 +6,8 @@ from flask import Flask, request, jsonify, Response
 from flask_sqlalchemy import SQLAlchemy
 from faker import Faker
 import random
+import requests
+import json
 from datetime import date
 
 app=Flask(__name__)
@@ -16,23 +18,48 @@ db=SQLAlchemy(app)
 
 class Contact(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    firstname = db.Column(db.String(100))
-    lastname= db.Column(db.String(100))
-    phone = db.Column(db.String(100))
-    email= db.Column(db.String(100)) 
-    job= db.Column(db.String(100))
+    firstname = db.Column(db.String(500))
+    lastname = db.Column(db.String(500))
+    phone = db.Column(db.String(500))
+    email = db.Column(db.String(500))
+    address = db.Column(db.String(500))
+    dob = db.Column(db.Date)
+    picture = db.Column(db.String(500))
+    job = db.Column(db.String(500))
 
-    def __init__(self, firstname, lastname, phone, email, job):
+    def __init__(self, firstname, lastname, phone, email, address, dob, picture, job):
         self.firstname = firstname
         self.lastname = lastname
         self.phone = phone
         self.email = email
+        self.address = address
+        self.dob = dob
+        self.picture = picture
         self.job = job 
 
 fake = Faker()
 def populate_table():
     for n in range(0, 50):
-        new_contact=Contact(fake.first_name(), fake.last_name(), fake.phone_number(), fake.email(), fake.job())
+        result = requests.get("https://randomuser.me/api/")
+        data = result.text
+        parse_json = json.loads(data)
+
+        firstname = parse_json['results'][0]['name']['first']
+        lastname = parse_json['results'][0]['name']['last']
+        phone = parse_json['results'][0]['phone']
+        email = parse_json['results'][0]['email']
+        address = str(parse_json['results'][0]['location']['street']['number']) + " " + str(parse_json['results'][0]['location']['street']['name']) + ", " + str(parse_json['results'][0]['location']['postcode']) + " " + str(parse_json['results'][0]['location']['city']) + "\n"+ str(parse_json['results'][0]['location']['state']) + " " + str(parse_json['results'][0]['location']['country'])
+        dob = parse_json['results'][0]['dob']['date'].split('T', 1)[0]
+        picture = parse_json['results'][0]['picture']['thumbnail']
+        job = fake.job()
+
+        new_contact=Contact(firstname, lastname, phone, email, address, dob, picture, job)
         nb_app=random.randint(1,4) 
         db.session.add(new_contact)
     db.session.commit()
+
+if __name__ == '__main__':
+    db.drop_all()
+    db.create_all()
+    populate_table()
+    app.run(host="0.0.0.0", port=8080, debug=True)
--- api/src/test.py
+++ api/src/test.py
@@ -4,10 +4,28 @@
 # fake = Faker()
 # print(fake.phone_number())
 
-
 #mettre das def populate et faire boucle avec
-from flask import request 
-result = request.get("https://randomuser.me/api/")  #si return 200 c'est bon
+from flask import request
+import requests
+import json
+result = requests.get("https://randomuser.me/api/")  #si return 200 c'est bon
+#print(result.status_code)
+
+data = result.text
+parse_json = json.loads(data)
+email = parse_json['results'][0]['picture']['thumbnail']
+print(email)
+print(data)
+
+"""import requests
+import json
+response_API = requests.get('https://api.covid19india.org/state_district_wise.json')
+#print(response_API.status_code)
+data = response_API.text
+parse_json = json.loads(data)
+active_case = parse_json['Andaman and Nicobar Islands']['districtData']['South Andaman']['active']
+print("Active cases in South Andaman:", active_case)
+print(data)"""
 
 # Contact(result[""])
-print(result.json())
+#print(result.json())
