402ec968d12130a88374b38bc8dc6056106697d7
sabrineTT <sabrine.thibert@esme.fr>
2022-05-27 23:24:35 +0200

    Interface

--- my-contact-app/src/contact-info/ContactItem.js
+++ my-contact-app/src/contact-info/ContactItem.js
@@ -17,27 +17,27 @@ export default function ContactItem(props) {
             </div>
             <div className={styles.container}>
                 <img src={props.data.picture} />
-                <span className={styles.label}>{props.data.lastname.toUpperCase()} {props.data.firstname}</span>
+                <span className={styles.title}>{props.data.lastname.toUpperCase()} {props.data.firstname}</span>
                 <div className={styles.info}>
                     <div className={styles.categories}>
-                        <span className={styles.label}>Mobile</span>
+                        <span className={styles.title}>Mobile</span>
                         <span className={styles.label}>{props.data.phone}</span>
                     </div>
                     <div className={styles.categories}>
-                        <span className={styles.label}>Email</span>
+                        <span className={styles.title}>Email</span>
                         <span className={styles.label}>{props.data.email}</span>
                     </div>
                     <div className={styles.categories}>
-                        <span className={styles.label}>Address</span>
+                        <span className={styles.title}>Address</span>
                         <span className={styles.label}>{address[0]}</span>
                         <span className={styles.label}>{address[1]}</span>
                     </div>
                     <div className={styles.categories}>
-                        <span className={styles.label}>Date of Birth</span>
+                        <span className={styles.title}>Date of Birth</span>
                         <span className={styles.label}>{props.data.dob}</span>
                     </div>
                     <div className={styles.categories}>
-                        <span className={styles.label}>Job</span>
+                        <span className={styles.title}>Job</span>
                         <span className={styles.label}>{props.data.job}</span>
                     </div>
                 </div>
